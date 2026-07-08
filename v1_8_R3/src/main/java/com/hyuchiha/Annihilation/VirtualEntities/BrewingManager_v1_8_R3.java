6a02444300f0db230a49409cee49973f55e77b94
FabricioAgurto <87247950+FabricioAgurto@users.noreply.github.com>
2026-07-08 18:27:02 -0500

    Add 1.8.8 brewing manager

---
+++ v1_8_R3/src/main/java/com/hyuchiha/Annihilation/VirtualEntities/BrewingManager_v1_8_R3.java
@@ -0,0 +1,16 @@
+package com.hyuchiha.Annihilation.VirtualEntities;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+public class BrewingManager_v1_8_R3 extends BrewingManager {
+
+  public BrewingManager_v1_8_R3(Plugin plugin) {
+    super(plugin);
+  }
+
+  @Override
+  VirtualBrewingStand createBrewingStand(Player player) {
+    return new VirtualBrewingStand_v1_8_R3(player);
+  }
+}
